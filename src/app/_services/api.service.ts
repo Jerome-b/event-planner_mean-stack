05f4c74c8515df74e1bda82d4f87fd45808a16c1
Jerome-b <bui.jerome@gmail.com>
2020-03-03 11:49:30 -0800

    Update api.service.ts

--- src/app/_services/api.service.ts
+++ src/app/_services/api.service.ts
@@ -8,7 +8,7 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 })
 
 export class ApiService {
-  baseUri = 'http://localhost:4000/api';
+  baseUri = 'http://localhost:8080/api';
   headers = new HttpHeaders().set('Content-Type', 'application/json');
 
   constructor(private http: HttpClient) { }
