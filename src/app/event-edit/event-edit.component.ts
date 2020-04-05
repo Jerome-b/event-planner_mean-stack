40d6f459feaf5c0817aac0a0a2ff57892a5bc37c
Jerome-b <49626404+Jerome-b@users.noreply.github.com>
2020-04-04 23:22:01 -0700

    retrieve forms from database

--- src/app/event-edit/event-edit.component.ts
+++ src/app/event-edit/event-edit.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from '../_services/api.service';
 import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
-// import { Event } from '../models/event';
+import { Event } from '../models/event';
+
 
 @Component({
   selector: 'app-event-edit',
@@ -11,41 +12,34 @@ import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 })
 export class EventEditComponent implements OnInit {
 
-  submitted = false;
-  editForm: FormGroup;
-  // eventData: Event[];
-  drinkSizeSelection: any = ['fl.oz', 'L', 'mL'];
-
   constructor(
     public fb: FormBuilder,
+    public fb2: FormBuilder,
+    public fb3: FormBuilder,
+    public fb4: FormBuilder,
     private actRoute: ActivatedRoute,
     private apiService: ApiService,
     private router: Router
   ) { }
 
+  submitted = false;
+  editForm: FormGroup;
+  drinkSizeSelection: any = ['fl.oz', 'L', 'mL', 'gal'];
+  foodSizeSelection: any = ['lbs', 'kg', 'g', 'oz', 'qt'];
+  event: Event;
+
   ngOnInit() {
-    this.updateEvent();
     const id = this.actRoute.snapshot.paramMap.get('id');
     this.getEvent(id);
     this.editForm = this.fb.group({
       name: ['', [Validators.required]],
-      description: ['', [Validators.required]],
+      date: ['', [Validators.required]],
+      time: ['', [Validators.required]],
       address: ['', [Validators.required]],
-      drinkName: ['', [Validators.required]],
-      drinkSize: ['', [Validators.required]],
-      drinkSizeNumber: ['', [Validators.required]],
-      drinkQuantity: ['', [Validators.required]],
-      test: this.fb.array([this.fb.group({
-        blabla: '',
-
-      })])
-    });
-  }
-
-  // Choose options with select-dropdown
-  updateEventForm(e) {
-    this.editForm.get('drinkSize').setValue(e, {
-      onlySelf: true
+      description: ['', [Validators.required]],
+      drink: this.fb.array([]),
+      food: this.fb.array([]),
+      object: this.fb.array([]),
     });
   }
 
@@ -54,41 +48,95 @@ export class EventEditComponent implements OnInit {
     return this.editForm.controls;
   }
 
-  get blablaPoints() {
-    return this.editForm.get('test') as FormArray;
+  get drinkForm() {
+    return this.editForm.get('drink') as FormArray;
+  }
+
+  get foodForm() {
+    return this.editForm.get('food') as FormArray;
+  }
+
+  get objectForm() {
+    return this.editForm.get('object') as FormArray;
   }
 
   getEvent(id) {
-    this.apiService.getEvent(id).subscribe(data => {
-      this.editForm.setValue({
-        name: data.name,
-        description: data.description,
-        address: data.address,
-        drinkName: data.drinkName,
-        drinkSize: data.drinkSize,
-        drinkSizeNumber: data.drinkSizeNumber,
-        drinkQuantity: data.drinkQuantity,
-        test: {
-          blabla: data.blabla
-        }
-      });
+    this.apiService.getEvent(id).subscribe({
+      next: (event: Event) => {
+        this.event = event;
+        this.displayEvent(event);
+    }});
+  }
+
+  // Add form group dynamically
+  addDrink() {
+    this.drinkForm.push(this.fb2.group({
+      drinkName: '',
+      drinkSize: '',
+      drinkSizeNumber: '',
+      drinkQuantity: '',
+    }));
+  }
+
+  addFood() {
+    this.foodForm.push(this.fb3.group({
+      foodName: '',
+      foodSize: '',
+      foodSizeNumber: '',
+      foodQuantity: '',
+    }));
+  }
+
+  addObject() {
+    this.objectForm.push(this.fb4.group({
+      objectName: '',
+      objectQuantity: '',
+    }));
+  }
+
+  // Delete form group dynamically
+  delDrink(index: number) {
+    const arrayControl = this.editForm.controls.drink as FormArray;
+    arrayControl.removeAt(index);
+  }
+
+  delFood(index: number) {
+    const arrayControl = this.editForm.controls.food as FormArray;
+    arrayControl.removeAt(index);
+  }
+
+  delObject(index: number) {
+    const arrayControl = this.editForm.controls.object as FormArray;
+    arrayControl.removeAt(index);
+  }
+
+  // Retrieve the 3 form group: drink, food and object
+  setFormgroup() {
+    const control = this.editForm.get('drink') as FormArray;
+    const control2 = this.editForm.get('food') as FormArray;
+    const control3 = this.editForm.get('object') as FormArray;
+    this.event.drink.forEach(test => {
+      control.push(this.fb.group(test));
+    });
+    this.event.food.forEach(test => {
+      control2.push(this.fb.group(test));
+    });
+    this.event.object.forEach(test => {
+      control3.push(this.fb.group(test));
     });
   }
 
-  updateEvent() {
-    this.editForm = this.fb.group({
-      name: ['', [Validators.required]],
-      description: ['', [Validators.required]],
-      address: ['', [Validators.required]],
-      drinkName: ['', [Validators.required]],
-      drinkSize: ['', [Validators.required]],
-      drinkSizeNumber: ['', [Validators.required]],
-      drinkQuantity: ['', [Validators.required]],
-      test: this.fb.array([this.fb.group({
-        blabla: '',
-
-      })])
+  // Display the event form to edit
+  displayEvent(event: Event): void {
+    this.editForm.patchValue({
+      name: event.name,
+      date: event.date,
+      time: event.time,
+      address: event.address,
+      description: event.description,
     });
+
+    this.setFormgroup();
   }
 
   onSubmit() {
