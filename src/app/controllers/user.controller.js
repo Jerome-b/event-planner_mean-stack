59a4002bd18cc56aa9cc8d0130b1d04d9e608a68
Jerome-b <49626404+Jerome-b@users.noreply.github.com>
2020-04-28 19:00:33 -0700

    controllers

--- src/app/controllers/user.controller.js
+++ src/app/controllers/user.controller.js
@@ -2,7 +2,33 @@ const db = require("../models");
 const User = db.user;
 
 exports.findAll = (req, res) => {
-  User.find({}, {username: 1, email: 1, _id: 1})
+  User.find({}, {firstname: 0, lastname: 0, phone: 0, password: 0, __v: 0})
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Error retrieving users."
+      });
+    });
+}
+
+exports.findAll2 = (req, res) => {
+  User.find({}, {_id: 0, firstname: 0, lastname: 0, email:0, phone: 0, password: 0, __v: 0})
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Error retrieving users."
+      });
+    });
+}
+
+exports.findAll3 = (req, res) => {
+  User.find({}, {_id: 0, firstname: 0, lastname: 0, phone: 0, password: 0, __v: 0})
     .then(data => {
       res.send(data);
     })
