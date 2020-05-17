7f634933973e1cfaaedd30f2290e4b6f5caddad2
Jerome-b <49626404+Jerome-b@users.noreply.github.com>
2020-05-16 18:00:48 -0700

    update

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user.model';
 import { Observable } from 'rxjs';
 
-const API_URL = '/api/user/';
+const API_URL = 'http://www.evplan.net/api/user/';
 
 @Injectable({
   providedIn: 'root'
